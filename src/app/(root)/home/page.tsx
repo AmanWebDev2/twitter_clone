096c11ab745142a65b6fadcc761ba27bdbe52a9c
aman <aman@oaza.energy>
2024-04-25 22:43:51 +0530

    initiate project

---
+++ src/app/(root)/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='grid grid-cols-12 min-h-screen'>
+        <div className="col-span-3"></div>
+        <div className="col-span-6 border-r-[0.5px] border-l-[0.5px] border-r-slate-500"></div>
+        <div className="col-span-3"></div>
+    </div>
+  )
+}
+
+export default page
